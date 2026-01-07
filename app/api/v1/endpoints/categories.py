d3495f876d6ff60490ae2e3122b728e3ba467819
Erika Flores <foglzerika@gmail.com>
2026-01-07 00:56:55 -0600

    feat: create post category endpoint

--- app/api/v1/endpoints/categories.py
+++ app/api/v1/endpoints/categories.py
@@ -10,3 +10,18 @@ router = APIRouter()
 def get_categories(db: Session = Depends(get_db)):
     categories = db.query(Category).all()
     return {"categories": categories}
+
+@router.post("/", response_model=categorie_schemas.CategorieResponse)
+def create_category(
+    category: categorie_schemas.CategorieCreate,
+    db: Session = Depends(get_db)
+):
+    db_category = db.query(Category).filter(Category.name == category.name).first()
+    if db_category:
+        raise HTTPException(status_code=400, detail="Category already exists")
+    
+    new_category = Category(name=category.name)
+    db.add(new_category)
+    db.commit()
+    db.refresh(new_category)
+    return {"category": new_category}
