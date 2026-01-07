68eee9f5e559663c3fb5b7ddbb94a9aba283ecb1
Erika Flores <foglzerika@gmail.com>
2026-01-07 11:40:13 -0600

    feat : Create endpoint for auth

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -34,12 +34,36 @@ def register(
         obj_in=user_in,
         hashed_password=hashed_password,
     )
+@router.post("/login", response_model=auth_schemas.TokenResponse)
+def login(
+    db: Session = Depends(get_db),
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> Any:
+    user = user_repository.get_by_email(db, email=form_data.username)
+
+    if not user or not security.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=security.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    token = security.create_access_token(
+        subject=user.id, expires_delta=access_token_expires
+    )
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "user": user
+    }
+
 @router.post("/login/access-token", response_model=auth_schemas.TokenResponse)
 def login_access_token(
     db: Session = Depends(get_db),
     form_data: OAuth2PasswordRequestForm = Depends(),
 ) -> Any:
-  user = user_repository.get_by_email(db, email=form_data.email)
+  user = user_repository.get_by_email(db, email=form_data.username)
 
   if not user or not security.verify_password(form_data.password, user.hashed_password):
        raise HTTPException(
@@ -47,7 +71,7 @@ def login_access_token(
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
        )
-  
+
   access_token_expires = timedelta(minutes=security.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
   token = security.create_access_token(
             subject=user.id, expires_delta=access_token_expires
--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class settings():
-    JWT_SECRET_KEY :str  = os.getenv("SECRET_KEY", "change_in_production")
-    JWT_ALGORITHM : str ="HS256"
+    JWT_SECRET :str  = os.getenv("SECRET_KEY", "change_in_production")
+    JWT_ALG: str ="HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
     API_V1_STR: str = "/api/v1"
 
