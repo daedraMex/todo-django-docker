9ee4a7f0435753f75c5029aacfff2b3a3fcdef05
Erika Flores <foglzerika@gmail.com>
2026-01-06 22:09:46 -0600

    feat: add params for pagination tasks

--- app/api/v1/endpoints/tasks.py
+++ app/api/v1/endpoints/tasks.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter,Query,Body, HTTPException,Path
 from app.schemas import task as task_schemas
-from typing import List,Optional
+from typing import List,Optional, Literal
 router = APIRouter()
 
 TASKS = [
     {"id": 1, "title": "Task 1", "description": "Description for Task 1", "is_completed": False},
     {"id": 2, "title": "Task 2", "description": "Description for Task 2", "is_completed": True},
+    {"id": 3, "title": "Task 3", "description": "Description for Task 3", "is_completed": False},
+    {"id": 4, "title": "Task 4", "description": "Description for Task 4", "is_completed": True},
+    {"id": 5, "title": "Task 5", "description": "Description for Task 5", "is_completed": False},
+    {"id": 6, "title": "Task 6", "description": "Description for Task 6", "is_completed": True},
+    {"id": 7, "title": "Task 7", "description": "Description for Task 7", "is_completed": False},
+    {"id": 8, "title": "Task 8", "description": "Description for Task 8", "is_completed": True},
+    {"id": 9, "title": "Task 9", "description": "Description for Task 9", "is_completed": False},
+    {"id": 10, "title": "Task 10", "description": "Description for Task 10", "is_completed": True},
 ]
 
 @router.get("/", response_model=List[task_schemas.TaskResponse])
@@ -16,16 +24,41 @@ def get_tasks(query: Optional[str] = Query(
     min_length=3,
     max_length=50,
     regex="^[a-zA-Z]+$",
-    )):
+    ),
+    limit : int = Query(
+        6,
+        ge=1,
+        le=50,
+        description="Maximum number of tasks to return by page"
+    ),
+    offset : int = Query(
+        0,
+        ge=0,
+        le=1000,
+        description="Number of tasks to skip for pagination" 
+    ), 
+    order_by: Literal["id"]  = Query(
+        "id",
+        description="Field to order tasks by"
+    ),
+    direction: Literal["asc","desc"] = Query(
+        "asc",
+        description="Order direction"),
+    ):
+    filtered_data = TASKS
     if query:
-      filtered_tasks = []
-      for task in TASKS:
-         if query.lower() in task["title"].lower():
-            filtered_tasks.append(task)
-        
-      return  filtered_tasks
+        filtered_data = [t for t in TASKS if query.lower() in t["title"].lower()]
     
-    return TASKS
+    is_reverse = (direction == "desc")
+    sorted_data = sorted(
+        filtered_data, 
+        key=lambda x: x[order_by], 
+        reverse=is_reverse
+    )
+    
+    results = sorted_data[offset : offset + limit]
+    
+    return results
 
 
 @router.post("/")
